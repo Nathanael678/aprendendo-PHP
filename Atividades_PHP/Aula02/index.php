93b0ed5a2e4b23ee6e86eed5b0867a2b389be68b
NATHANAEL ALVES RODRIGUES <nathanaelalves786@gmail.com>
2026-04-18 19:29:27 -0300

    Add files via upload

---
+++ Atividades_PHP/Aula02/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 2 - POO</title>
+    </head>
+    <body>
+        <?php
+            require_once 'Caneta.php';
+            $c1 = new Caneta;
+            $c1->cor = "Azul";
+            $c1->ponta = 0.5;
+            $c1->tampada = true;
+            $c1->destampar();
+            print_r($c1);
+            
+            $c1->rabiscar();
+            
+            $c2 = new Caneta;
+            $c2->cor = "verde";
+            $c2->carga = 50;
+            $c2->tampar();
+            print_r($c2);
+            
+            $c2->rabiscar();
+        ?>
+    </body>
+</html>
